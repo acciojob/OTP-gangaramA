e27bc8f40daac6c3b85acafbdc0f56fff23d6a30
gangaramA <150507677+gangaramA@users.noreply.github.com>
2024-08-23 19:30:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+    code.addEventListener('keydown', (e) => {
+        if (e.key >= 0 && e.key <= 9) {
+            codes[idx].value = ''; // Clear the current box before adding the new number
+            setTimeout(() => codes[idx + 1]?.focus(), 10);
+        } else if (e.key === 'Backspace') {
+            setTimeout(() => codes[idx - 1]?.focus(), 10);
+        }
+    });
+});
